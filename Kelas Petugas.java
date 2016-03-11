ea3e50d6989604ba9e9ec61059819f128651e5d5
basado97 <basadositorus@gmail.com>
2016-03-11 21:44:57 +0700

    kelas Petugas

---
+++ Kelas Petugas.java
@@ -0,0 +1,39 @@
+public class Petugas {
+	private long Id;
+	private String Pass;
+	
+	public class Petugas (String name,long id) {
+			nama = new.nama;
+	}
+	
+	public long id() {
+			this.id = Id;
+	}
+	
+	public catatBarang(Barang b,Gudang g) {
+			this.Barang = Barang;
+			this.Gudang = Gudang;
+	}
+	
+	public editBarang(Barang b,Gudang g){
+			this.Barang = Barang;
+			this.Gudang = Gudang;
+	}
+	
+	public Laporan (Gudang g){
+			this.Gudang = Gudang;
+	}
+	
+	public Tampilinfo(String kodeBarang,Gudang g) {
+			kodeBarang = new kodeBarang;
+			this.Gudang = Gudang;
+	}
+	
+	public setPass(String Pass){
+			this.Pass = Pass;
+	}
+	
+	public String getPass() {
+			return Pass;
+	}
+}
